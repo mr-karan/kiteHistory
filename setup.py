27044acfc8bd35f7510827d6572dc2e484808a2a
Karan Sharma <karansharma1295@gmail.com>
2017-06-11 10:40:01 +0530

    fix module error

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (IOError, ImportError):
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-version = '1.3'
+version = '1.3.1'
 
 setup(
     name='kitehistory',
